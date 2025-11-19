0a4b285f5786f918fbab5899fccba945fe8cb0e5
otterclonefy <otter@clonefy.ai>
2025-11-19 21:06:45 +0100

    Create clonefy.js

---
+++ public/clonefy.js
@@ -0,0 +1,9 @@
+console.log("Clonefy Player Script Loaded");
+
+// Dit laat alleen iets zien op de pagina, puur als test:
+document.body.insertAdjacentHTML(
+  "beforeend",
+  `<div style="padding:20px;background:#eef;border:1px solid #99c;margin-top:20px;">
+     Clonefy.js werkt! (test)
+   </div>`
+);
