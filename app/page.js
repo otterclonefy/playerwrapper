013aa027794f98286c3421eb7a5f9afa9cd1b610
otterclonefy <otter@clonefy.ai>
2025-11-19 21:35:48 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,6 +3,14 @@ export default function Home() {
     <div style={{ padding: "40px", fontSize: "20px" }}>
       <h1>Clonefy Player Wrapper</h1>
       <p>De basis staat. We kunnen nu verder bouwen.</p>
+
+      {/* Embed test */}
+      <div
+        data-embed-id="test123"
+        style={{ marginTop: "40px", padding: "20px", border: "2px dashed #999" }}
+      >
+        Loading embed...
+      </div>
     </div>
   );
 }
