ea4b436b46480c97a438c8f06e673f19b2adba41
otterclonefy <otter@clonefy.ai>
2025-11-19 20:46:11 +0100

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "40px", fontSize: "20px" }}>
+      <h1>Clonefy Player Wrapper</h1>
+      <p>De basis staat. We kunnen nu verder bouwen.</p>
+    </div>
+  );
+}
